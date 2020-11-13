49e04190b72fcd4163f942fbd49a25314c1dc196
Calebe Rocha <angelo.calebe@edu.pucrs.br>
2020-11-13 01:06:01 -0300

    Tipos de dados em parse_config

--- config_parser.py
+++ config_parser.py
@@ -3,5 +3,5 @@ def parse_config(config_file):
     with open(config_file, "r") as f:
         for row in f.readlines():
             row = row.replace("\r", "").replace("\n", "").split(" ")
-            nodes[int(row[0])] = {"host": row[1], "port": row[2], "chance": row[3]}
+            nodes[int(row[0])] = {"host": row[1], "port": int(row[2]), "chance": float(row[3])}
     return nodes

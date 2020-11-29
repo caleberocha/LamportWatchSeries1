7844de424770b35b1338544f2fb64b3a07d45a57
Calebe Rocha <angelo.calebe@edu.pucrs.br>
2020-11-29 17:15:47 -0300

    clearlogs

--- utils/clearlogs.sh
+++ utils/clearlogs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 for i in 1 2 3 4 5; do
-    curl http://dontpad.com/calebe/lamport/$i \
-        -H 'Content-Type: application/x-www-form-urlencoded;charset=UTF-8' \
-        --data-raw 'text=' \
-        --compressed \
-        --insecure
+    echo Clearing log $i
+    curl http://dontpad.com/calebe/lamport/$i -H 'Content-Type: application/x-www-form-urlencoded;charset=UTF-8' --data-raw 'text='
+    echo ''
 done
